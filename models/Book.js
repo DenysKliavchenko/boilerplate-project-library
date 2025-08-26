63043304b7df153acdeaf572c962a189e0b12871
DenysKliavchenko <d.klyavchenko@gmail.com>
2025-08-26 10:05:52 +0300

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
 
+const BookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    comments: { type: [String], default: [] }
+  },
+  { versionKey: false }
+);
+
+BookSchema.virtual('commentcount').get(function () {
+  return this.comments.length;
+});
+BookSchema.set('toJSON', { virtuals: true });
+BookSchema.set('toObject', { virtuals: true });
+
+module.exports = mongoose.model('Book', BookSchema);
